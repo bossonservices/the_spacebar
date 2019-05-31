826a4e7a30edb4ef8de1d2d76ee304404eff719c
bossonservices <info@bosson-services.com>
2019-05-31 13:51:08 +0200

    new service

---
+++ src/Services/MarkdownHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Services;
+
+
+use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+
+class MarkdownHelper
+{
+    private $cache;
+    private $markdown;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger)
+    {
+        $this->cache = $cache;
+        $this->markdown = $markdown;
+        $this->logger = $markdownLogger;
+    }
+
+    public function parse(string $source): string
+    {
+        if (stripos($source, 'bacon') != false)
+        {
+            $this->logger->info('They are talking about bacon again!');
+        }
+
+        $item = $this->cache->getItem('markdown_'.md5($source));
+        if (!$item->isHit()) {
+            $item->set($this->markdown->transform($source));
+            $this->cache->save($item);
+        }
+            return $item->get();
+    }
+}
